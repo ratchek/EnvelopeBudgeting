713d5521e66a19e9b659ba493937e9fe3659c9dd
ratchek <git@ratchek.com>
2024-02-24 12:36:56 -0500

    Add envelope, fill, and transaction models

--- envelopes/api/admin.py
+++ envelopes/api/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserChangeForm, CustomUserCreationForm
-from .models import CustomUser
+from .models import CustomUser, Envelope, Fill, Transaction
 
 
 class CustomUserAdmin(UserAdmin):
@@ -16,3 +16,6 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Envelope)
+admin.site.register(Transaction)
+admin.site.register(Fill)
--- envelopes/api/models.py
+++ envelopes/api/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CustomUser(AbstractUser):
@@ -7,3 +8,37 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+# An envelope is a budget category. It has a name and a total amount of money in it.
+class Envelope(models.Model):
+    name = models.CharField(max_length=50)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+# A transaction is a record of money being spent from an envelope. It has a date, an amount, a name, notes, and a reference to the envelope it came from.
+class Transaction(models.Model):
+    date = models.DateField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    name = models.CharField(max_length=100)
+    notes = models.TextField()
+    envelope = models.ForeignKey(Envelope, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
+
+
+# A fill is a record of money being added to an envelope. It has a date, an amount, a name, notes, and a reference to the envelope it came from.
+class Fill(models.Model):
+    date = models.DateField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    name = models.CharField(max_length=100)
+    notes = models.TextField()
+    envelope = models.ForeignKey(Envelope, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
