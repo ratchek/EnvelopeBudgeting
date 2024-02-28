a5f12e2ddf3e23d3445dcaaf7da40909ff549b0c
ratchek <git@ratchek.com>
2024-02-27 19:53:01 -0500

    Add a basic view at the root

--- envelopes/api/urls.py
+++ envelopes/api/urls.py
@@ -4,12 +4,17 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path("envelopes/", views.EnvelopeList.as_view()),
-    path("envelopes/<int:pk>/", views.EnvelopeDetail.as_view()),
-    path("transactions/", views.TransactionList.as_view()),
-    path("transactions/<int:pk>/", views.TransactionDetail.as_view()),
-    path("fills/", views.FillList.as_view()),
-    path("fills/<int:pk>/", views.FillDetail.as_view()),
+    path("envelopes/", views.EnvelopeList.as_view(), name="envelope-list"),
+    path("envelopes/<int:pk>/", views.EnvelopeDetail.as_view(), name="envelope-detail"),
+    path("transactions/", views.TransactionList.as_view(), name="transaction-list"),
+    path(
+        "transactions/<int:pk>/",
+        views.TransactionDetail.as_view(),
+        name="transaction-detail",
+    ),
+    path("fills/", views.FillList.as_view(), name="fill-list"),
+    path("fills/<int:pk>/", views.FillDetail.as_view(), name="fill-detail"),
+    path("", views.api_root),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- envelopes/api/views.py
+++ envelopes/api/views.py
@@ -1,7 +1,21 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from .models import Envelope, Fill, Transaction
 from .serializers import EnvelopeSerializer, FillSerializer, TransactionSerializer
+
+
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response(
+        {
+            "envelopes": reverse("envelope-list", request=request, format=format),
+            "transactions": reverse("transaction-list", request=request, format=format),
+            "fills": reverse("fill-list", request=request, format=format),
+        }
+    )
 
 
 class EnvelopeList(generics.ListCreateAPIView):
