e6ffd41da040a80be7671898fd975305d10a4c0e
ratchek <git@ratchek.com>
2024-03-04 15:35:22 -0500

    Reference auth user properly

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator
 from django.db import models
@@ -15,7 +16,7 @@ class CustomUser(AbstractUser):
 class Envelope(models.Model):
     name = models.CharField(max_length=50)
     total = models.DecimalField(max_digits=10, decimal_places=2)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
