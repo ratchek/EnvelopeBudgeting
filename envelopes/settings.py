c8f70d2c527d7e3ab4703dbe7d0167998eb5d62a
ratchek <git@ratchek.com>
2024-04-26 15:33:19 -0400

    Add basic authentication back to production settings

--- envelopes/settings.py
+++ envelopes/settings.py
@@ -129,12 +129,15 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 3,
 }
 
 if DEVELOPMENT_MODE:
     REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"] += [
-        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
     ]
 
 # Internationalization
